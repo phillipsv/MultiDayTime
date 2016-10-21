3706e461f3eb94a23cde93bc8ec58b901c960956
Vineet Phillips <vineet.phillips@mrmiomp1649.rci.rogers.ca>
2016-10-21 11:48:28 -0400

    Added timepicker for improved date selection

--- src/mdt.jquery.js
+++ src/mdt.jquery.js
@@ -30,6 +30,7 @@
         return this.charAt(0).toUpperCase() + this.slice(1);
     };
 
+
     MultiDayTime.prototype = {
         constructor: MultiDayTime,
         init: function () {
@@ -66,9 +67,27 @@
             });
 
             _self.$element.on("click", "." +  _self.remove_li_class, function () {
+                //cleanup the timepicker plugin
+                $(this).siblings("."+_self.select_start_time_class).timepicker("remove");
+                $(this).siblings("."+_self.select_end_time_class).timepicker("remove");
                 $(this).parent().remove();
             });
 
+        },
+        padZero: function(n, width) {
+            var z = 0;
+            width = width || 2;
+            n  = n + '';
+            return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+        },
+        makeid: function (){
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for( var i=0; i < 5; i++ )
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return text;
         },
 
         drawInput: function (entry) {
@@ -80,13 +99,26 @@
                 entry.end_time='';
             }
 
+            //id for start and end strings to be used for timepicker, easier to instantiate.
+            var start_id = "start_time_"+this.makeid();
+            var end_id = "end_time_"+this.makeid();
+
             this.$element.find("." + this.input_container_ul_class).append("<div class='" + this.input_container_li_class + "'>" +
                 this.generateDaySelect(entry.day) +
-                "<input type='text' class='" + this.select_start_time_class + "' placeholder='HH:MM:SS' value='"+entry.start_time+"'/> - " +
-                "<input type='text' class='" + this.select_end_time_class + "' placeholder='HH:MM:SS' value='"+entry.end_time+"'/>" +
+                "<input type='text' id='" + start_id + "' class='" + this.select_start_time_class + "' "+
+                " value='"+entry.start_time+"'/> - " +
+                "<input id='" + end_id + "' type='text' class='" + this.select_end_time_class + "' "+
+                " value='"+entry.end_time+"'/>" +
                 "<span class='" + this.remove_li_class + "'><a href='#'>Remove</a></span></div>");
-        },
 
+            //instantiate time picker for the input boxes.
+            this.invokeTimepicker(start_id);
+            this.invokeTimepicker(end_id);
+
+        },
+        invokeTimepicker: function(id){
+            $("#"+id).timepicker();
+        },
         drawButtons: function () {
             this.$element.find("." + this.button_panel_class).append("<a href='#' class='" + this.button_panel_add_class + "'>Add Time</a>");
         },
@@ -139,9 +171,13 @@
                     var tmp = {},
                         day = $.trim($(this).children("." +  _self.select_day_class).eq(0).val());
 
-                    tmp.start_time = $.trim($(this).children("." +  _self.select_start_time_class).eq(0).val());
-                    tmp.end_time = $.trim($(this).children("." +  _self.select_end_time_class).eq(0).val());
+                    start_time_obj = $(this).children("." +  _self.select_start_time_class).eq(0).timepicker("getTime");
+                    end_time_obj = $(this).children("." +  _self.select_end_time_class).eq(0).timepicker("getTime");
 
+                    tmp.start_time = _self.padZero( start_time_obj.getHours() )+":"+ _self.padZero(start_time_obj.getMinutes())+":"+ _self.padZero(start_time_obj.getSeconds());
+                    tmp.end_time =  _self.padZero(end_time_obj.getHours())+":"+ _self.padZero(end_time_obj.getMinutes())+":"+ _self.padZero(end_time_obj.getSeconds());
+
+                    console.log(tmp);
                     if(!_self.validateTime(tmp.start_time) || !_self.validateTime(tmp.end_time)){
                         error_message = "Invalid time values";
                         return false;
